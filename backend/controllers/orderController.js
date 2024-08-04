4958db5b7ce69320f519d49888691f9ba4020589
Rudraneel Dutta <its.rudraneel@gmail.com>
2024-08-04 16:50:13 +0530

    stripe integrated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,7 @@ const placeOrder = async (req, res) => {
       line_items: line_items,
       mode: "payment",
       success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
     });
 
     res.json({ success: true, session_url: session.url });
