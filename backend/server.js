4130138faf499cc08b64b768f5dd60d9d83642e9
Rudraneel Dutta <its.rudraneel@gmail.com>
2024-08-07 12:49:07 +0530

    cron alive

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRouter from './routes/userRoute.js';
 import 'dotenv/config';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import axios from 'axios';
 
 
 
@@ -31,6 +32,21 @@ app.get('/', (req, res) => {
     res.send('API Working')
 })
 
+// self ping
+app.get('/ping', (req, res) => {
+    res.status(200).send('Ping received');
+});
+
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`)
+
+    // Ping itself every 14 minutes
+    setInterval(async () => {
+        try {
+            await axios.get('https://ecom-backend-6w8m.onrender.com/ping');
+            console.log('Ping sent to self');
+        } catch (error) {
+            console.error('Error pinging self:', error.message);
+        }
+    }, 14 * 60 * 1000); // 14 minutes    
 })
